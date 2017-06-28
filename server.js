4a40c9f27a4e8aefdf4ad88fe0ac21f446a8c42e
Orion Wolf_Hubbard <owolfhu1@gmail.com>
2017-06-27 22:33:17 -0400

    game ui refactor, needs to set dom back when game ends

--- server.js
+++ server.js
@@ -238,7 +238,7 @@ io.on('connection', socket => {
         //if someone played a face/ace
         if (game.facePlayer !== 'none') {
             //if player plays a face/ace
-            if (card[0] > 10 || card[0] === 1) {
+            if ((card[0] > 10 || card[0] === 1) && game[player].cards[0][1] !== 'joker') {///code change check it
                 //change facePlayer and set triesLeft
                 game.facePlayer = player;
                 if (card[0] === 1) game.triesLeft = 4;
